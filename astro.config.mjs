688e5c2496fedf0e2bd9e78b3671892988e703d2
kaminzhi <issac20050710@gmail.com>
2024-05-26 06:25:20 +0800

    feat: 🎸 url change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kaminzhi.github.io",
+  site: "https://www.kaminzhi.com",
   integrations: [mdx(), sitemap()],
 });
