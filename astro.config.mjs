2af6658bf234c9e6b2d775d39249e578efdf1bc1
kaminzhi <issac20050710@gmail.com>
2024-05-26 15:59:17 +0800

    feat: 🎸 add tailwild and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-
 import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.kaminzhi.com",
-  integrations: [mdx(), sitemap(), react(), image()]
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()],
 });
+
