48f803c0b990561224d0b2d1f2b6768893b72242
kaminzhi <issac20050710@gmail.com>
2024-05-31 12:46:44 +0800

    fix: 🐛 back to last version && add Menu

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "kamin_zhi";
-export const SITE_DESCRIPTION = "Life is suck, but I'm still here.";
-export const HOMEPAGE_URL = "https://www.kaminzhi.com";
+export const SITE_TITLE = 'kamin_zhi'
+export const SITE_DESCRIPTION = "Life Suck, But I'm Still Here."
+export const HOMEPAGE_URL = 'https://www.kaminzhi.com'
